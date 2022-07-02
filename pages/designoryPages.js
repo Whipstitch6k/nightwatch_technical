b97e3168de87b0371945948c8142e74e20cf5552
Whipstitch6k <reese.lec@protonmail.com>
2022-07-02 18:30:49 -0500

    modified designoryPages, removed url modified deisgnery_tests, completed menuVerificationTest

--- pages/designoryPages.js
+++ pages/designoryPages.js
@@ -1,5 +1,4 @@
 module.exports = {
-  url: 'https://www.designory.com/',
   elements:{
     menuButton:{
       selector: 'div[id=nav-toggle]' //alternatively, maybe there is a way to click the upper-right-most button? But this one works for now
--- tests/designery_tests.js
+++ tests/designery_tests.js
@@ -14,45 +14,52 @@ describe('Designory Tests', function() {
     Verify that all six options take the user to relevant pages.
     Verify that on those relevant pages menu options are the same.
   */
+
+  //Note: It might be nice to be able to dynamically create the list of menuItems rather than hardcode them. Might see about figuring out this feature if time permits
   it.only('menuVerificationTest', function(browser) {
-    browser
-      var menuItemsWithLinks = ["WORK", "ABOUT", "CAREERS", "CONTACT", "NEWS"] //all menu items except LOCATIONS, since LOCATIONS is a submenu and the rest are links
-      var designory = browser.page.designoryPages();
 
-      designory.navigate()
-        //for the sake of getting it working, I'm going to do visibility checks before and after clicking the menu button. If an element is not visible before the click and is visible after the click, logically it is part of the menu. There may be a way to check the actual element nesting, however I am having difficult locating this capability at this time. As such for the sake of getting the test written and working I'm going to do the visiblity checks mentioned above. I will return to try and figure out how to check the nesting if time permits
+    menuVerifcationTestSinglePage('');
+    menuVerifcationTestSinglePage('/work');
+    menuVerifcationTestSinglePage('/about');
+    menuVerifcationTestSinglePage('/carees');
+    menuVerifcationTestSinglePage('/contact');
+    menuVerifcationTestSinglePage('/news');
 
+    function menuVerifcationTestSinglePage(suffix){
+      browser
+        var menuItemsWithLinks = ["WORK", "ABOUT", "CAREERS", "CONTACT", "NEWS"] //all menu items except LOCATIONS, since LOCATIONS is a submenu and the rest are links
+        var designory = browser.page.designoryPages();
 
-        //verify menu not visible before menu click.
-        .verify.not.visible('@menuWrapper')
-
-        //verify menu items not visible before menu click
-        .useXpath() // text selectors are Xpath
-        .verify.not.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
-        for(var i=0; i<menuItemsWithLinks.length; i++){
-          browser.verify.not.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
-        }
-        browser.useCss()
-
-        //menu click
-        designory.verify.visible('@menuButton') //uncertain why, but above code block breaks everything below unless I lead the next statement with "designory". 
-        .click('@menuButton')
-
-        //verify menu options visible after click, also check that link matches for all except LOCATIONS
-        .verify.visible('@menuWrapper')
-
-        .useXpath() // text selectors are Xpath
-        .verify.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
-        for(var i=0; i<menuItemsWithLinks.length; i++){
-          browser.verify.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
-          browser.expect.element("//a[text()='" + menuItemsWithLinks[i] + "']").to.have.attribute('href').which.contains(menuItemsWithLinks[i].toLowerCase()); //for example, "WORK" sould link to "/work"
-        }
-        browser.useCss()
+        designory.navigate('https://www.designory.com' + suffix)
+          //for the sake of getting it working, I'm going to do visibility checks before and after clicking the menu button. If an element is not visible before the click and is visible after the click, logically it is part of the menu. There may be a way to check the actual element nesting, however I am having difficult locating this capability at this time. As such for the sake of getting the test written and working I'm going to do the visiblity checks mentioned above. I will return to try and figure out how to check the nesting if time permits
 
 
-        /*
-        breaking for lunch. At this point I am satisfied on the tests with the designory page. We check for visibility of all elements post-click. Need to perform the same test for each webpage. Either see if we can turn this into a function and loop for each webpage or see if we can call menuVeriicationTest with multiple browser vars navigated to the different sites.
-        */
+          //verify menu not visible before menu click.
+          .verify.not.visible('@menuWrapper')
+
+          //verify menu items not visible before menu click
+          .useXpath() // text selectors are Xpath
+          .verify.not.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
+          for(var i=0; i<menuItemsWithLinks.length; i++){
+            browser.verify.not.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
+          }
+          browser.useCss()
+
+          //menu click
+          designory.verify.visible('@menuButton') //uncertain why, but above code block breaks everything below unless I lead the next statement with "designory". 
+          .click('@menuButton')
+
+          //verify menu options visible after click, also check that link matches for all except LOCATIONS
+          .verify.visible('@menuWrapper')
+
+          .useXpath() // text selectors are Xpath
+          .verify.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
+          for(var i=0; i<menuItemsWithLinks.length; i++){
+            browser.verify.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
+            browser.expect.element("//a[text()='" + menuItemsWithLinks[i] + "']").to.have.attribute('href').which.contains(menuItemsWithLinks[i].toLowerCase()); //for example, "WORK" sould link to "/work"
+          }
+          browser.useCss()
+      }
   });
 
   /*
