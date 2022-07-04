d11cc0d6dbc66c915b2c3a1fe6df38fd876bbae8
Whipstitch6k <reese.lec@protonmail.com>
2022-07-03 21:58:45 -0500

    updated logging

--- tests/designery_tests.js
+++ tests/designery_tests.js
@@ -30,32 +30,40 @@ describe('Designory Tests', function() {
         var menuItemsWithLinks = ["WORK", "ABOUT", "CAREERS", "CONTACT", "NEWS"] //all menu items except LOCATIONS, since LOCATIONS is a submenu and the rest are links
         var designory = browser.page.designoryPages();
 
+        browser.getLog('browser', function() {console.log("\n\n")})
         designory.navigate('https://www.designory.com' + suffix)
           //Using xpath navigation, I could check to verify the menuItems are actually part of the menu, but based on my current understanding of xpath, I can only verify for a known element structure. There may be a more generic way but at current I do not know. For the time being I will stay with the visibility check method, as the logic still holds. If the element is not visible before clicking the menu button and then visible after clicking the button, then the element is part of the menu
 
+
+          browser.getLog('browser', function() {console.log("\nPerforming visibility checks before clicking menu button:")})
+
           //verify menu not visible before menu click.
-          .verify.not.visible('@menuWrapper')
+          designory.verify.not.visible('@menuWrapper', 'menuWrapper not visible')
 
           //verify menu items not visible before menu click
           .useXpath() // text selectors are Xpath
-          .verify.not.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
+          .verify.not.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']", "LOCATIONS menu item not visible") //locations has a submenu and not a link, test by itself
           for(var i=0; i<menuItemsWithLinks.length; i++){
-            browser.verify.not.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
+            browser.verify.not.visible("//a[text()='" + menuItemsWithLinks[i] + "']", menuItemsWithLinks[i] + " menu item not visible") 
           }
           browser.useCss()
 
+
+          browser.getLog('browser', function() {console.log("\nClicking menu button:")})
           //menu click
-          designory.verify.visible('@menuButton') //uncertain why, but above code block breaks everything below unless I lead the next statement with "designory". Perhaps something about the switch to xpath and then back to css removes designory from scope?
+          designory.verify.visible('@menuButton', "menuButton visible")
           .click('@menuButton')
 
+          browser.getLog('browser', function() {console.log("\nPerforming visibility/link checks after clicking menu button:")})
           //verify menu options visible after click, also check that link matches for all except LOCATIONS
-          .verify.visible('@menuWrapper')
+          designory.verify.visible('@menuWrapper', "menuWrapper visible")
 
           .useXpath() // text selectors are Xpath
-          .verify.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']") //locations has a submenu and not a link, test by itself
+          .verify.visible("//a[contains(@class, 'subnav-toggle') and text()='LOCATIONS']", "LOCATIONS menu item visible") //locations has a submenu and not a link, test by itself
           for(var i=0; i<menuItemsWithLinks.length; i++){
-            browser.verify.visible("//a[text()='" + menuItemsWithLinks[i] + "']") 
-            browser.expect.element("//a[text()='" + menuItemsWithLinks[i] + "']").to.have.attribute('href').which.contains(menuItemsWithLinks[i].toLowerCase()); //for example, "WORK" sould link to "/work"
+            browser.verify.visible("//a[text()='" + menuItemsWithLinks[i] + "']", menuItemsWithLinks[i] + " menu item visible") 
+            var linkMessage = menuItemsWithLinks[i] + " menu item links to /" + menuItemsWithLinks[i].toLowerCase()
+            browser.expect.element("//a[text()='" + menuItemsWithLinks[i] + "']").to.have.attribute('href', linkMessage).which.contains(menuItemsWithLinks[i].toLowerCase()); //for example, "WORK" sould link to "/work"
           }
           browser.useCss()
       }
@@ -69,18 +77,23 @@ describe('Designory Tests', function() {
   */
   it('cookieVerificationTest', function(browser) {
     cookieNoticeWithButtonPress('@acceptCookieButton')
+    cookieNoticeWithButtonPress('@acceptCookieButton') //performing twice to verify clearing cookies works
     //cookieNoticeWithButtonPress('@cookieMessageXButton') //there is no x button, awaiting info from Designory
 
     function cookieNoticeWithButtonPress(buttonSelector){
       browser
+        browser.getLog('browser', function() {console.log("\n")})
         var designory = browser.page.designoryPages();
         designory.navigate('https://www.designory.com')
 
+        browser.getLog('browser', function() {console.log("  - Deleting cookies")})
         .deleteCookies(function() {});
+        browser.getLog('browser', function() {console.log("  - Refreshing page")})
         browser.refresh()
-        designory.verify.visible('@cookieMessage')
-        .click(buttonSelector)
-        .verify.not.visible('@cookieMessage')
+        designory.verify.visible('@cookieMessage', "Cookie message visible")
+        browser.getLog('browser', function() {console.log("  - clicking " + buttonSelector)})
+        designory.click(buttonSelector)
+        .verify.not.visible('@cookieMessage', "Cookie message not visible")
       }
   });
 
@@ -98,32 +111,34 @@ describe('Designory Tests', function() {
       //doing this as a function for future-proofing. We can run it on other locations easier this way
     function locationVerificationTestWithArgs(city, h1Value, phoneNumber, mapUrl){
       browser
+        browser.getLog('browser', function() {console.log("\n")})
         var designory = browser.page.designoryPages();
         designory.navigate('https://www.designory.com/locations/' + city.toLowerCase())
         
         //verify the a element with text "Chicago" and href /locations.chicago is visible
         .useXpath() // text selectors are Xpath
-        browser.verify.visible("//a[text()='" + city + "']") 
-        browser.expect.element("//a[text()='" + city + "']").to.have.attribute('href').which.contains(city.toLowerCase())
+        browser.verify.visible("//a[text()='" + city + "']", "Verify '" + city + "' visible") 
+        var linkMessage = "Verify '" + city + "' links to /" + city.toLowerCase()
+        browser.expect.element("//a[text()='" + city + "']").to.have.attribute('href', linkMessage).which.contains(city.toLowerCase())
         
         //verify that element is part of the footer. Note: this looks for a very specific known element structure. Maybe there is a way to make it more generic
-        browser.verify.elementPresent("//a[text()='" + city + "']//parent::h3/parent::div/parent::div/parent::div/parent::div/parent::div/parent::footer")
+        browser.verify.elementPresent("//a[text()='" + city + "']//parent::h3/parent::div/parent::div/parent::div/parent::div/parent::div/parent::footer", "Verify '" + city + "' is located in footer")
         browser.useCss()
 
         //vefify it is in the footer via xpath navigation
 
-        .verify.textEquals('h1', h1Value)
+        .verify.textEquals('h1', h1Value, "Verify H1 value is " + h1Value)
 
         //phone element is above the Area of Interest form picker, which is the only form-picker on the page
         const phoneElement = locateWith(By.tagName('P')).above(By.className('form-picker'))
-        browser.verify.textEquals(phoneElement, "Phone: " + phoneNumber)
+        browser.verify.textEquals(phoneElement, "Phone: " + phoneNumber, "Verify phone number is: Phone: " + phoneNumber)
 
         //h2 font size
-        .verify.cssProperty('h2', "font-size", "40px") //gets 16px. This is not a static value so it will change based on window size and the like
+        .verify.cssProperty('h2', "font-size", "40px", "Verify H2 font size 40px") //gets 16px. This is not a static value so it will change based on window size and the like
 
         //map url
         .useXpath() //test is xpath
-        browser.expect.element("//a[contains(@class, 'location')]").to.have.attribute('href').which.equals("http://maps.google.com/?q=%20225%20N%20Michigan%20Ave,%20Suite%202100%20Chicago,%20IL%2060601") //returns correct but not percent encoded
+        browser.expect.element("//a[contains(@class, 'location')]").to.have.attribute('href', "Verify map link").which.equals("http://maps.google.com/?q=%20225%20N%20Michigan%20Ave,%20Suite%202100%20Chicago,%20IL%2060601") //returns correct but not percent encoded
         browser.useCss();
       }
   });
